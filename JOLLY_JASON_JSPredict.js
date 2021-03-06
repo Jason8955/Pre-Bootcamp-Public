e24ad22edbe82b639547983170786a9fcd569f58
Jason Jolly <73667332+Jason8955@users.noreply.github.com>
2021-03-05 18:40:50 -0800

    Add files via upload

---
+++ JOLLY_JASON_JSPredict.js
@@ -0,0 +1,8 @@
+//I was born in 
+//I was born in 1980
+//Summing Numbers!, num1 is 10, num2 is 20, 30
+console.log("/Summing Numbe", /Summing Numbe)
+
+
+console.log(jason);
+
---
+++ function calculateTipAmount (billTotal,.js
@@ -0,0 +1,9 @@
+
+
+
+function numPlus(num) {
+    console.log('num is', num);
+    return num+15;
+}
+var result = numPlus(3);
+console.log('x is', result);
